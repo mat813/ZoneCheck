d77043394ed9be4cdd1797f1188a2b8b57228efb
Stéphane D'Alu <sdalu>
2002-08-05 17:10:53 +0000

    Initial revision

---
+++ lib/address/ipv4.rb
@@ -0,0 +1,78 @@
+class Address
+    ##
+    ##
+    ##
+    class IPv4 < Address
+	Regex = /\A(\d+)\.(\d+)\.(\d+)\.(\d+)\z/
+	
+	def self.is_valid(str)
+	    str =~ Regex
+	end
+
+	def self.create(arg)
+	    case arg
+	    when IPv4
+		return arg
+	    when Regex
+		if (0..255) === (a = $1.to_i) &&
+		   (0..255) === (b = $2.to_i) &&
+		   (0..255) === (c = $3.to_i) &&
+		   (0..255) === (d = $4.to_i)
+		    return self.new([a, b, c, d].pack("CCCC"))
+		else
+		    raise InvalidAddress, 
+			"IPv4 address with invalid value: #{arg}"
+		end
+	    else
+		raise InvalidAddress, 
+		    "can't interprete as IPv4 address: #{arg.inspect}"
+	    end
+	end
+	
+	def initialize(address)
+	    unless address.kind_of?(String) && address.length == 4
+		raise InvalidAddress, "IPv4 address must be 4 bytes"
+	    end
+	    @address = address
+	end
+	
+	def private?
+	    # 10.0.0.0        -   10.255.255.255  (10/8 prefix)
+	    # 172.16.0.0      -   172.31.255.255  (172.16/12 prefix)
+	    # 192.168.0.0     -   192.168.255.255 (192.168/16 prefix)
+	    bytes = @address.unpack("CCCC")
+	    return (((bytes[0] == 10))                            ||
+		    ((bytes[0] == 172) && (bytes[1]&0xf0 == 16))  ||
+		    ((bytes[0] == 192) && (bytes[1] == 168)))
+	end
+
+	def prefix(size=nil)
+	    if size.nil?
+		# TODO
+		raise RuntimeError, "Not Implemented Yet"
+	    else
+		if size > @address.size * 8
+		    raise ArgumentError, "prefix size too big"
+		end
+		bytes, bits = size / 8, size % 8
+		address = @address.slice(0, bytes) + 
+		    ("\0" * (@address.size - bytes))
+		address[bytes] = (@address[bytes] >> bits) << bits
+		Resolv::IPv4::new(address)
+	    end
+	end
+
+	def to_s
+	    return sprintf("%d.%d.%d.%d", *@address.unpack("CCCC"))
+	end
+	
+	def inspect
+	    return "#<#{self.class} #{self.to_s}>"
+	end
+	
+	def to_name
+	    return DNS::Name::create('%d.%d.%d.%d.in-addr.arpa.' % 
+				     @address.unpack('CCCC').reverse)
+	end
+    end
+end
---
+++ lib/address/ipv6.rb
@@ -0,0 +1,192 @@
+# $Id$
+
+class Address
+    ##
+    ##
+    ##
+    class IPv6 < Address
+	private
+	Regex_8Hex = /\A
+            (?:[0-9A-Fa-f]{1,4}:){7}
+	    [0-9A-Fa-f]{1,4}
+            \z/x
+	
+	Regex_CompressedHex = /\A
+            ((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?) ::
+            ((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?)
+            \z/x
+	
+	Regex_6Hex4Dec = /\A
+            ((?:[0-9A-Fa-f]{1,4}:){6,6})
+            (\d+)\.(\d+)\.(\d+)\.(\d+)
+            \z/x
+	
+	Regex_CompressedHex4Dec = /\A
+            ((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?) ::
+            ((?:[0-9A-Fa-f]{1,4}:)*)
+            (\d+)\.(\d+)\.(\d+)\.(\d+)
+            \z/x
+
+	Regex_4Dec = /\A
+            (\d+)\.(\d+)\.(\d+)\.(\d+)
+            \z/x
+
+	public
+	IPv6Regex = /
+            (?:#{Regex_8Hex.source})              |
+            (?:#{Regex_CompressedHex.source})     |
+            (?:#{Regex_6Hex4Dec.source})          |
+            (?:#{Regex_CompressedHex4Dec.source})
+            /x
+
+	IPv6StrictRegex = /
+            (?:#{Regex_8Hex.source})              |
+            (?:#{Regex_CompressedHex.source})
+            /x
+
+	IPv6LooseRegex = /
+            (?:#{Regex_8Hex.source})              |
+            (?:#{Regex_CompressedHex.source})     |
+            (?:#{Regex_6Hex4Dec.source})          |
+            (?:#{Regex_CompressedHex4Dec.source}) |
+            (?:#{Regex_4Dec.source})
+            /x
+	
+	Regex = IPv6Regex
+
+
+
+	def self.hex_pack(str, data="")
+	    str.scan(/[0-9A-Fa-f]+/) { |hex| data << [hex.hex].pack('n') }
+	    data
+	end
+
+	def self.is_valid(str, opt=Regex)
+	    str =~ opt
+	end
+
+	def self.create(arg, opt=Regex)
+	    case arg
+	    when IPv6
+		return arg
+	    when IPv4
+		return self.create("::ffff:#{arg.to_s}")
+	    when String
+		address = ''
+
+		# According to the option, select the test that
+		# should be performed
+		test = if    opt == IPv6StrictRegex then 1
+		       elsif opt == IPv6Regex       then 2
+		       elsif opt == IPv6LooseRegex  then 3
+		       else  raise ArgumentError, "unknown option"
+		       end
+
+		# Test: a:b:c:d:e:f:g:h
+		if    (test >= 1) && Regex_8Hex             =~ arg
+		    hex_pack(arg, address)
+
+		# Test: a:b:c::d:e:f
+		elsif (test >= 1) && Regex_CompressedHex    =~ arg
+		    prefix, suffix = $1, $2
+		    a1 = hex_pack(prefix)
+		    a2 = hex_pack(suffix)
+		    omitlen = 16 - a1.length - a2.length
+		    address << a1 << "\0" * omitlen << a2
+
+		# Test: a:b:c:d:e:f:g.h.i.j
+		elsif (test >= 2) && Regex_6Hex4Dec =~ arg
+		    prefix, a, b, c, d = $1, $2.to_i, $3.to_i, $4.to_i, $5.to_i
+		    if (0..255) === a && (0..255) === b && 
+		       (0..255) === c && (0..255) === d
+			hex_pack(prefix, address)
+			address << [a, b, c, d].pack('CCCC')
+		    end
+
+		# Test: a::b:c:d.e.f.g
+		elsif (test >= 2) && Regex_CompressedHex4Dec =~ arg
+		    prefix, suffix, a, b, c, d = $1, $2, $3.to_i, $4.to_i, $5.to_i, $6.to_i
+		    if (0..255) === a && (0..255) === b && 
+		       (0..255) === c && (0..255) === d
+			a1 = hex_pack(prefix)
+			a2 = hex_pack(suffix)
+			omitlen = 12 - a1.length - a2.length
+			address << a1 << "\0" * omitlen << a2 << [a, b, c, d].pack('CCCC')
+		    end
+
+		# Test: a.b.c.d
+		elsif (test >= 3) && Regex_4Dec              =~ arg
+		    a, b, c, d = $1.to_i, $2.to_i, $3.to_i, $4.to_i
+		    if (0..255) === a && (0..255) === b && 
+		       (0..255) === c && (0..255) === d
+			address << "\0" * 10 << "\377" * 2 << [a, b, c, d].pack('CCCC')
+		    end
+		end
+
+		# Check if conversion succed
+		if address.length != 16
+		    raise InvalidAddress, 
+			"IPv6 address with invalid value: #{arg}"
+		end
+
+		# Return new address
+		return IPv6::new(address)
+	    else
+		raise InvalidAddress,
+		    "can't interprete as IPv6 address: #{arg.inspect}"
+	    end
+	end
+	
+	def initialize(address)
+	    unless address.kind_of?(String) && address.length == 16
+		raise InvalidAddress, "IPv6 address must be 16 bytes"
+	    end
+	    @address = address
+	end
+	
+	def private?
+	    return false
+	end
+
+	def prefix(size=nil)
+	    if size.nil?
+		prefix(64)
+	    else
+		if size > @address.size * 8
+		    raise StandardError, "prefix size too big"
+		end
+		bytes, bits = size / 8, size % 8
+		address = @address.slice(0, bytes) + 
+		    ("\0" * (@address.size - bytes))
+		address[bytes] = (@address[bytes] >> bits) << bits
+		Resolv::IPv6::new(address)
+	    end
+	end
+
+	def to_s
+	    address = sprintf("%X:%X:%X:%X:%X:%X:%X:%X", 
+			      *@address.unpack("nnnnnnnn"))
+	    unless address.sub!(/(^|:)0(:0)+(:|$)/, '::')
+		address.sub!(/(^|:)0(:|$)/, '::')
+	    end
+	    return address
+	end
+	
+	def inspect
+	    return "#<#{self.class} #{self.to_s}>"
+	end
+	
+	def to_name
+	    # ip6.arpa should be searched too. [RFC3152]
+	    return DNS::Name.new(@address.unpack("H32")[0].split(//).reverse +
+				 ['ip6', 'int'])
+	end
+
+	class Compatibility < IPv6
+	    def self.create(arg, opt=IPv6LooseRegex)
+		IPv6::create(arg, opt)
+	    end
+	end
+
+    end
+end
