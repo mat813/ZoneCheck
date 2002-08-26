101cf4418ee5617eab1b8f28837334686845acf2
Stéphane D'Alu <sdalu>
2002-08-26 22:31:42 +0000

    *** empty log message ***

--- lib/nresolv/transport.rb
+++ lib/nresolv/transport.rb
@@ -17,25 +17,62 @@ require 'thread'
 require 'fcntl'
 require 'timeout'
 
+#
+# requester = Requester::ConnectedUDP::new(address)
+# query     = requester.query(msg)
+# reply     = query.send.wait
+#
+
 module NResolv
+    ##
+    ## Timeout Errors
+    ##
+    class NResolvTimeout < StandardError
+    end
+
     class DNS
+	Port		 = 53
 	DNSThreadGroup = ThreadGroup::new
 
+	TCPTimeout	 = 5
         UDPRetrySequence = [ 5, 10, 20, 40 ]
-	UDPSize = 512
+	UDPSize		 = 512
 
 	##
 	##
 	##
 	class Requester
+	    attr_reader :sock
+
 	    def initialize
                 @queries = {}
             end
             
-            def delete(query)
-                @queries.delete(query)
+            def delete(id)
+                @queries.delete(id)
             end
+	    
+	    def query(msg)
+		unless msg.type == Message::Query
+		    raise RuntimeError, "DNS message should be a query"
+		end
+		@queries[msg.msgid] = create_query(msg)
+	    end
 
+	    def receive(data)
+		begin
+		    msg = Message::from_wire(data)
+		    if q = @queries[msg.msgid]
+			q.recv msg
+		    else
+			STDERR.print("non-handled DNS message id=#{msg.msgid}")
+		    end
+		rescue Message::DecodeError
+		   STDERR.print("DNS message decoding error: #{reply.inspect}")
+		end
+	    end
+
+	    # close the requester and all the pending queries
             def close
                 thread, sock, @thread, @sock = @thread, @sock
                 begin
@@ -44,13 +81,55 @@ module NResolv
                         thread.join
                     end
                 ensure
-                    sock.close if sock
+		    if sock
+			@queries.each { |id, query| query.close }
+			sock.close
+		    end
                 end
             end
             
 
             class Query
-            end
+		attr_reader :msgid
+
+		# two queries are identical if they have the same message id.
+		def eql?(other)
+		    (self.type == other.type) && (self.msgid == other.msgid)
+		end
+		alias eql? ==
+		    
+		# return a hash value
+		def hash
+		    msgid.hash
+		end
+
+		# wait for an answer or a timeout (_NResolvTimeout_ exception).
+		#
+		# The timeout value can be specified by _tout_, if its
+		# +nil+ a sensible default value is used.
+		def wait(tout=nil)
+		    tout = @dflttout if tout.nil?
+		    begin
+			timeout(tout) { return @queue.pop }
+		    rescue TimeoutError
+			raise NResolvTimeout
+		    ensure
+			close
+		    end
+		end
+
+		# receive an answer message
+		def recv(msg)
+		    close
+		    @queue.push(msg)
+		end
+
+		# close the query
+		def close
+		    @requester.delete(self)
+		end
+
+           end
 
 
 
@@ -64,42 +143,36 @@ module NResolv
                     @port = port
                     @sock = TCPSocket.new(host, port)
                     @sock.fcntl(Fcntl::F_SETFD, 1)
-                    @id = -1
                     @thread = Thread.new {
                         DNSThreadGroup.add Thread.current
                         loop {
-                            lenhdr = @sock.read(2)
-                            len    = lenhdr.unpack('n')[0]
-                            reply  = @sock.read(len)
-                            msg = begin
-                                      Message.decode(reply)
-                                  rescue DecodeError
-                                      STDERR.print("DNS message decoding error: #{reply.inspect}")
-                                      next
-                                  end
-                            if q = @queries[msg.id]
-                                q.recv msg
-                            end
+			    lenhdr = @sock.read(2)
+			    len    = lenhdr.unpack('n')[0]
+			    reply  = @sock.read(len)
+			    receive(reply)
                         }
                     }
                 end
                 
-                def query(msg, data)
-                    id = Thread.exclusive { @id = (@id + 1) & 0xffff }
-                    packet      = msg.encode
-                    packet[0,2] = [packet.length, id].pack('nn')
-                    return @queries[id] = Query::new(packet, data, @sock)
-                end
-                
+		def create_query(msg)
+		    Query::new(msg, self)
+		end
+
+		private
                 class Query < Requester::Query
-                    def initialize(msg, data, sock)
-                        super(data)
-                        @msg = msg
-                        @sock = sock
+                    def initialize(msg, requester)
+			@requester = requester
+ 			@queue     = Queue::new
+                        @rawmsg    = msg.to_wire
+			@msgid     = msg.msgid
+			@pktlen    = [@rawmsg.length].pack('n')
+                        @sock      = requester.sock
+			@dflttout  = 5
                     end
                     
                     def send
-                        @sock.write(@msg)
+			@sock.write(@pktlen)
+                        @sock.write(@rawmsg)
                         @sock.flush
                         self
                     end
@@ -119,41 +192,28 @@ module NResolv
                     @sock   = UDPSocket::new
                     @sock.connect(host, port)
                     @sock.fcntl(Fcntl::F_SETFD, 1)
-                    @id     = -1
                     @thread = Thread::new {
                         DNSThreadGroup.add Thread.current
                         loop {
-                            reply = @sock.recv(UDPSize)
-                            msg = begin
-                                      Message::from_wire(reply)
-                                  rescue DecodeError
-                                      STDERR.print("DNS message decoding error: #{reply.inspect}")
-                                      next
-                                  end
-                            if q = @queries[msg.msgid]
-                                q.recv msg
-                            else
-                                STDERR.print("non-handled DNS message: #{msg.msgid}")
-                            end
+			    reply = @sock.recv(UDPSize)
+			    receive(reply)
                         }
                     }
                 end
                 
-                def query(msg)
-                    return @queries[msg.msgid] = Query::new(msg, @sock)
+                def create_query(msg)
+                    Query::new(msg, self)
                 end
 
                 class Query < Requester::Query
-                    def initialize(msg, sock)
-			@queue  = Queue::new
-                        @rawmsg = msg.to_wire
-                        @sock   = sock
-                    end
-
-                    def wait
-                        tout = 2
-                        UDPRetrySequence.each { |timeout| tout += timeout }
-                        timeout(tout) { return @queue.pop }
+                    def initialize(msg, requester)
+			@requester = requester
+			@queue     = Queue::new
+                        @rawmsg    = msg.to_wire
+ 			@msgid     = msg.msgid
+                        @sock      = requester.sock
+			@dflttout  = 2
+			UDPRetrySequence.each { |tout| @dflttout += tout }
                     end
 
                     def send
@@ -168,15 +228,15 @@ module NResolv
                         self
                     end
 
-                    def recv(msg)
-                        @thread.kill
-                        @queue.push(msg)
-                    end
+		    def close
+			thread, @thread = @thread
+			if thread
+			    thread.kill
+			end
+			super()
+		    end
                 end
             end
-
 	end
     end
 end
-
-
