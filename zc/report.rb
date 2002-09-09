45ce85026dec19057bf1e1a21d7126b9310bfb69
Stéphane D'Alu <sdalu>
2002-09-09 15:40:57 +0000

    *** empty log message ***

--- zc/report.rb
+++ zc/report.rb
@@ -1,44 +1,105 @@
 # $Id$
 
 module Diagnostic
-    class Output
+    class FatalError < StandardError
     end
 
-    class Fatal < Output
-	def addmsg(msg)
-	    printf $mc.get("fatal"), msg
-	    return true
+    ##
+    ## Consolidation of reported messages
+    ##
+    class Consolidation
+	class Processor # ABSTRACT
+	    attr_reader :count
+
+	    def initialize(data)
+		@data = data
+		@count = 0
+	    end
+
+	    def add_answer(answer)
+		@data[[answer.testname, answer.ns, answer.ip]] = [self, answer]
+	    end
 	end
-    end
-    
-    class Warning < Output
+	
+	class FatalAnswer < Processor
+	    def add_answer(answer)
+		super(answer, answer.explanation)
+		raise FatalError unless answer.ok?
+	    end
+	end
+	
+	class WarningAnswer < Processor
+	    def add_answer(answer)
+		super(answer, answer.explanation)
+	    end
+	end
+	
+	class InfoAnswer < Processor
+	    def add_answer(answer)
+		super(answer)
+	    end
+	end
+
+
 	def initialize
-	    @@count = 0
-	    @@fatal = false
+	    @data = { }
+
+	    @fatal   = FatalAnswer::new(@data)
+	    @warning = WarningAnswer::new(@data)
+	    @info    = InfoAnswer::new(@data)
 	end
-	def addmsg(msg)
-	    printf $mc.get("warning"), msg
-	    return @@fatal
-	end
-	def fatal=(fatal)
-	    @@fatal = fatal
-	end
-	def count
-	    @@count
-	end
+
+	attr_reader :fatal, :warning, :info
     end
 
-    class Info < Output
-	def initialize
-	    @@count = 0
+
+    ##
+    ## Straight interpretation of messages.
+    ##
+    class Straight
+	class Processor # ABSTRACT
+	    attr_reader :count
+
+	    def initialize(formatter)
+		@formatter = formatter
+		@count     = 0
+	    end
+
+	    def add_answer(answer, formatter_method=nil)
+		if !answer.ok?
+		    @count += 1
+		    formatter_method.call(answer) if formatter_method
+		end
+	    end
 	end
-	def addmsg(msg)
-	    @@count += 1
-	    printf $mc.get("info"), msg
-	    return false
+	
+	class Fatal < Processor
+	    def add_answer(answer) 
+		super(answer, @formatter.method(:fatal))
+		raise FatalError unless answer.ok?
+		true
+	    end
 	end
-	def count
-	    @@count
+	
+	class Warning < Processor
+	    def add_answer(answer)
+		super(answer, @formatter.method(:warning))
+	    end
 	end
+	
+	class Info < Processor
+	    def add_answer(answer)
+		super(answer, @formatter.method(:info))
+	    end
+	end
+
+
+	def initialize(formatter)
+	    @fatal   = Fatal::new(formatter)
+	    @warning = Warning::new(formatter)
+	    @info    = Info::new(formatter)
+	end
+
+	attr_reader :fatal, :warning, :info
     end
 end
