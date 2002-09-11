a2a7fd5beba8e82294cf67d335f992cc9cf8d2d6
Stéphane D'Alu <sdalu>
2002-09-11 11:30:15 +0000

    Initial revision

---
+++ test/connectivity.rb
@@ -0,0 +1,72 @@
+# $Id$
+
+# 
+# AUTHOR : Stephane D'Alu <sdalu@nic.fr>
+# CREATED: 2002/08/02 13:58:17
+#
+# $Revivion$ 
+# $Date$
+#
+# CONTRIBUTORS:
+#
+#
+
+require 'framework'
+
+module CheckNetworkAddress
+    class Connectivity < Test
+	
+	#-- Tests ---------------------------------------------------
+	# DESC: Test TCP connectivity with DNS server
+	def chk_tcp(ns, ip)
+	    # The idea is to open a TCP connection
+	    #  if no one is listening          => Errno::ECONNREFUSED
+	    #  if there is a firewal (timeout) => Errno::EINVAL
+	    sock = nil
+	    begin
+		sock = TCPSocket::new(ip.to_s, NResolv::DNS::Port)
+		true
+	    rescue Errno::ECONNREFUSED, Errno::EINVAL
+		false
+	    ensure
+		sock.close unless sock.nil?
+	    end
+	end
+
+	# DESC: Test UDP connectivity with DNS server
+	def chk_udp(ns, ip)
+	    # The idea is to send 25 'server status' request in 5 seconds
+	    # if we received one answer (dont care about its content,
+	    # although generally 'Not Implemented') we consider it sucessful
+	    msg        = NResolv::DNS::Message::Query::new
+	    msg.opcode = NResolv::DNS::OpCode::STATUS
+	    rawmsg     = msg.to_wire
+	    sock       = nil
+	    thr        = nil
+	    begin
+		# Dirty hack for finding which protocol to use
+		protocol = ip.to_s =~ /:/ ? Socket::AF_INET6 \
+		                          : Socket::AF_INET
+
+		sock = UDPSocket::new(protocol)
+		sock.connect(ip.to_s, NResolv::DNS::Port)
+		thr = Thread::new {
+		    (1..25).each { sock.write(rawmsg) ; sleep(0.2) }
+		}
+		begin
+		    timeout(8) { sock.recv(1) }
+		    true
+		rescue TimeoutError
+		    false
+		end
+	    ensure
+		thr.kill   unless thr.nil?
+		sock.close unless sock.nil?
+	    end
+	end
+
+	def chk_icmp(ns, ip)
+	    true
+	end
+    end
+end
