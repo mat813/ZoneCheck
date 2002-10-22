4d917ad737bb3eb8cfdb1b4bdce6bb41626afa22
Stéphane D'Alu <sdalu>
2002-10-22 15:30:46 +0000

    fix due to param modifications

--- zc/input/cgi.rb
+++ zc/input/cgi.rb
@@ -46,17 +46,15 @@ class Param
 
 	def initialize
 	    @p    = Param::new
+	    @cgi  = ::CGI::new
 	end
 
 	def parse
-	    # CGI interpreter
-	    cgi = ::CGI::new
-	    
 	    # Lang
 	    # => The message catalogue need to be replaced
-	    if cgi["lang"].length == 1
+	    if @cgi["lang"].length == 1
 		begin
-		    lang = cgi["lang"][0]
+		    lang = @cgi["lang"][0]
 		    if $mc.available?(ZC_LANG_FILE, lang)
 			$mc.clear
 			$mc.lang = lang
@@ -67,72 +65,72 @@ class Param
 	    end
 
 	    # Batch
-	    if cgi.has_key?("batchdata")
-		@p.batch = BatchData::new(cgi["batchdata"][0])
+	    if @cgi.has_key?("batchdata")
+		@p.batch = BatchData::new(@cgi["batchdata"][0])
 	    end
 
 	    # Quiet, One
-	    @p.rflag.quiet = true if cgi.has_key?("quiet")
-	    @p.rflag.one   = true if cgi.has_key?("one")
+	    @p.rflag.quiet = true if @cgi.has_key?("quiet")
+	    @p.rflag.one   = true if @cgi.has_key?("one")
 
 
 	    # Verbose
-	    if cgi.has_key?("verbose")
-		@p.verbose = cgi["verbose"].join(",")
+	    if @cgi.has_key?("verbose")
+		@p.verbose = @cgi["verbose"].join(",")
 	    else
-		@p.verbose = "intro"            if cgi.has_key?("intro")
-		@p.verbose = "explain"          if cgi.has_key?("explain")
-		@p.verbose = cgi["progress"][0] if cgi.has_key?("progress")
+		@p.verbose = "intro"             if @cgi.has_key?("intro")
+		@p.verbose = "explain"           if @cgi.has_key?("explain")
+		@p.verbose = @cgi["progress"][0] if @cgi.has_key?("progress")
 	    end
 
 	    # Output
-	    if cgi.has_key?("output")
-		@p.output = cgi["output"].join(",")
+	    if @cgi.has_key?("output")
+		@p.output = @cgi["output"].join(",")
 	    else
-		@p.output = cgi["format"].join(",")
+		@p.output = @cgi["format"].join(",")
 	    end
 
 	    # Error
-	    if cgi.has_key?("error")
-		@p.error = cgi["error"].join(",")
+	    if @cgi.has_key?("error")
+		@p.error  = @cgi["error"].join(",")
 	    else
-		errorlvl  = cgi["errorlvl"].delete_if { |e| e =~ /^\s*$/ }
-		errorstop = cgi.has_key?("errorstop") ? "stop" : "nostop"
-		@p.error = (errorlvl + [ errorstop ]).join(",")
+		errorlvl  = @cgi["errorlvl"].delete_if { |e| e =~ /^\s*$/ }
+		errorstop = @cgi.has_key?("errorstop") ? "stop" : "nostop"
+		@p.error  = (errorlvl + [ errorstop ]).join(",")
 	    end
 
 	    # Transp
-	    if cgi.has_key?("transp")
-		@p.transp = cgi["transp"].join(",")
+	    if @cgi.has_key?("transp")
+		@p.transp = @cgi["transp"].join(",")
 	    else
-		@p.transp = (cgi["transp3"] + cgi["transp4"]).join(",")
+		@p.transp = (@cgi["transp3"] + @cgi["transp4"]).join(",")
 	    end
 
 	    # Category
-	    if cgi.has_key?("category")
-		@p.category = cgi["category"].join(",")
+	    if @cgi.has_key?("category")
+		@p.category = @cgi["category"].join(",")
 	    else
 		cat = [ ]
-		cat << "mail"  if cgi.has_key?("chkmail")
-		cat << "whois" if cgi.has_key?("chkwhois")
-		cat << "zone"  if cgi.has_key?("chkzone")
+		cat << "mail"  if @cgi.has_key?("chkmail")
+		cat << "whois" if @cgi.has_key?("chkwhois")
+		cat << "zone"  if @cgi.has_key?("chkzone")
 		if ! cat.empty?
 		    cat << "connectivity" << "dns"	# XXX: VERY BAD
-		    @p.category = cat.join(",")
+		    @p.test.categories = cat.join(",")
 		end
 	    end
 	    
 	    # NS and IPs
-	    if cgi.has_key?("ns")
-		@p.domain.ns = cgi["ns"].join(";")
+	    if @cgi.has_key?("ns")
+		@p.domain.ns = @cgi["ns"].join(";")
 	    else
 		ns_list = [ ]
 		(0..7).each { |i|
-		    next unless cgi_ns = cgi["ns#{i}"]
+		    next unless cgi_ns = @cgi["ns#{i}"]
 		    next unless cgi_ns.length > 0
 		    next if     (ns = cgi_ns[0]).empty?
 		    
-		    cgi_ips = cgi["ips#{i}"]
+		    cgi_ips = @cgi["ips#{i}"]
 		    if cgi_ips.nil? || cgi_ips.length == 0 
 			ns_list << [ ns ]
 		    else
@@ -156,16 +154,17 @@ class Param
 
 	    # Zone/Domain
 	    # XXX: todo check!!!
-	    @p.domain.name = cgi["zone"]
-
-	    # XXX: not good place
-	    puts cgi.header(@p.publisher_class::Mime)
+	    @p.domain.name = @cgi["zone"]
 
 	    # Ok
 	    @p
 	end
 
-	def interact(param)
+	def interact(config)
+	    # XXX: not good place
+	    @p.rflag.autoconf
+	    @p.publisher.autoconf(@p.rflag)
+	    puts @cgi.header(@p.publisher.engine.class::Mime)
 	    true
 	end
 
