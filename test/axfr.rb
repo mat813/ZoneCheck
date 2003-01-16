c5d1bd5474abbf4c51a29eb1e660bb4dcb63f339
Stéphane D'Alu <sdalu>
2003-01-16 10:25:23 +0000

    framework for AXFR tests

---
+++ test/axfr.rb
@@ -0,0 +1,40 @@
+# ZCTEST 1.0
+# $Id$
+
+# 
+# AUTHOR : Stephane D'Alu <sdalu@nic.fr>
+# CREATED: 2002/08/02 13:58:17
+#
+# $Revision$ 
+# $Date$
+#
+# CONTRIBUTORS:
+#
+#
+
+require 'framework'
+
+module CheckNetworkAddress
+    ##
+    ## Check domain NS records
+    ##
+    class AXFR < Test
+	with_msgcat "test/axfr.%s"
+
+	#-- Checks --------------------------------------------------
+	# DESC: Zone transfert is possible
+	def chk_axfr(ns, ip)
+	    true
+	end
+
+	# DESC: Zone transfert is not empty
+	def chk_axfr_empty(ns, ip)
+	    true
+	end
+
+	# DESC: Zone transfert containts only valid labels
+	def chk_axfr_valid_labels(ns, ip)
+	    true
+	end
+    end
+end
