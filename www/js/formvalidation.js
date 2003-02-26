b674533ad99491c9b97b629b7d9e339580cbd270
Stéphane D'Alu <sdalu>
2003-02-26 17:48:02 +0000

    cleanup html/javascript for form validation

---
+++ www/js/formvalidation.js
@@ -0,0 +1,48 @@
+// $Id$
+
+//
+// AUTHOR   : Stephane D'Alu <sdalu@nic.fr>
+// CREATED  : 2003/02/26 18:38:10
+//
+// COPYRIGHT: AFNIC (c) 2003
+// CONTACT  : zonecheck@nic.fr
+// LICENSE  : GPL v2.0
+//
+// $Revision$ 
+// $Date$
+//
+// CONTRIBUTORS:
+//
+//
+
+function zc_formvalid(form) {
+  if (form["zone"].value.length == 0) {
+    alert("Zone Empty");
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+function zc_formcheck(form) {
+  if (zc_formvalid(form))
+    form.submit();
+}	  
+
+function zc_formguess(form) {
+  form.action="";
+  form.method="get";
+  if (zc_formvalid(form))
+    form.submit();
+}
+
+function zc_formclear(form) {
+  var i;
+
+  if (form["zone"])
+    form["zone"].value = "";
+  for (i = 0 ; form["ns"+i] ; i++)
+    form["ns" +i].value = "";
+  for (i = 0 ; form["ips"+i] ; i++)
+    form["ips" +i].value = "";
+}
