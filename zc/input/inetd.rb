b53f156896b562d8602254d0204398986d953e8d
Stéphane D'Alu <sdalu>
2003-08-28 09:57:57 +0000

    inetd I18N/L10N

--- zc/input/inetd.rb
+++ zc/input/inetd.rb
@@ -42,10 +42,79 @@ require 'param'
 ## ----------------------------------------------------------------------
 ##
 ##
-
+## usage: PROGNAME: [-hV] [-46] [-c conf]
+##         --lang          Select another language (en, fr, ...)
+##     -h, --help          Show this message
+##     -V, --version       Display version and exit
+##     -c, --config        Specify location of the configuration file
+##         --testdir       Location of the directory holding tests
+##     -r, --resolver      Resolver to use for guessing 'ns' information
+##     -4, --ipv4          Only allow check the zone with IPv4 connectivity
+##     -6, --ipv6          Only allow check the zone with IPv6 connectivity
+##
+##
+## Command           Args
+##   ?|help           0     This message
+##   q|quit|exit      0     Leave ZoneCheck
+##   check            0     Launch the zone checking process
+##   zone|domain      1     Zone to test
+##   nslist           0+    Set the list of the zone nameservers
+##   set              1-2   Option to set (see options)
+##   unset            1     Option to unset (see options)
+##   preset           1     Preset configuration (supported: classic)
+## 
+## Options           Args
+##   lang             1     Select another language (en, fr, ...)
+##   one              0     Only display the most relevant message
+##   quiet            0     Don't display extra titles
+##   tagonly          0     Display only tag (suitable for scripting)
+##   category         1+    Only perform test for the specified category
+##   verbose          1+    Display extra information (see verbose)
+##   output           1+    Output (see output)
+##   error            1+    Behaviour in case of error (see error)
+##   transp           1+    Transport/routing layer (see transp)
+##   option           1+    Set extra options (-,-opt,opt,opt=foo)
+## 
+## Arguments for Options
+##   verbose:             [intro/testname/explain/details]
+##                        [reportok|fatalonly] [testdesc|counter]
+##     intro          [i]  Print summary for domain and associated nameservers
+##     testname       [n]  Print the test name
+##     explain        [x]  Print an explanation for failed tests
+##     details        [d]  Print a detailed description of the failure
+##     reportok       [o]  Still report passed test
+##     fatalonly      [f]  Print fatal errors only
+##     testdesc       [t]  Print the test description before running it
+##     counter        [c]  Print a test counter
+## 
+##   output:               [byseverity|byhost] [text|html]
+##     byseverity    *[bs] Output is sorted/merged by severity
+##     byhost         [bh] Output is sorted/merged by host
+##     text          *[t]  Output plain text
+##     html           [h]  Output HTML
+## 
+##   error:                [allfatal|allwarning|dfltseverity] [stop|nostop]
+##     allfatal       [af] All error are considered fatal
+##     allwarning     [aw] All error are considered warning
+##     dfltseverity  *[ds] Use the severity associated with the test
+##     stop          *[s]  Stop on the first fatal error
+##     nostop         [ns] Never stop (even on fatal error)
+## 
+##   transp:               [ipv4/ipv6] [udp|tcp|std]
+##     ipv4          *[4]  Use IPv4 routing protocol
+##     ipv6          *[6]  Use IPv6 routing protocol
+##     udp            [u]  Use UDP transport layer
+##     tcp            [t]  Use TCP transport layer
+##     std           *[s]  Use UDP with fallback to TCP for truncated messages
+## 
+## Example
+##   preset classic
+##   zone foorbar.com
+##   check
+## 
 module Input
     class INETD
-	with_msgcat "cgi.%s"
+	with_msgcat "inetd.%s"
 
 	def initialize
 	    @prompt = "zonecheck> "
@@ -53,6 +122,8 @@ module Input
 
 	def parse(p)
 	    begin
+		ipv6, ipv4 = false, false
+
 		opts = GetoptLong::new(
 		[ '--help',	'-h',	GetoptLong::NO_ARGUMENT       ],
 		[ '--version',	'-V',	GetoptLong::NO_ARGUMENT       ],
@@ -64,7 +135,7 @@ module Input
 		[ '--ipv4',	'-4',	GetoptLong::NO_ARGUMENT       ],
 		[ '--ipv6',	'-6',	GetoptLong::NO_ARGUMENT       ] )
 
-		opts.each do |opt, arg|
+		opts.each { |opt, arg|
 		    case opt
 		    when '--help'      then usage(EXIT_USAGE, $console.stdout)
 		    when '--version'
@@ -72,43 +143,46 @@ module Input
 			l10n_version.gsub!(/PROGNAME/, PROGNAME)
 			$console.stdout.puts l10n_version
 			exit EXIT_OK
-		    when '--quiet'     then p.rflag.quiet		= true
+		    when '--quiet'     then p.rflag.quiet	= true
 		    when '--debug'     then $dbg.level		= arg
 		    when '--lang'
 			if $mc.available?(ZC_LANG_FILE, arg)
 			    $mc.lang = arg
 			    $mc.reload
 			end
-		    when '--config'    then p.fs.cfgfile		= arg.untaint
-		    when '--testdir'   then p.fs.testdir		= arg.untaint
+		    when '--config'    then p.fs.cfgfile	= arg.untaint
+		    when '--testdir'   then p.fs.testdir	= arg.untaint
 		    when '--resolver'  then p.resolver.local	= arg
-		    when '--ipv6'      then p.network.ipv6		= true
-		    when '--ipv4'      then p.network.ipv4		= true
+		    when '--ipv6'      then ipv6		= true
+		    when '--ipv4'      then ipv4		= true
 		    end
-		end
+		}
 	    rescue GetoptLong::Error
 		return false
 	    end
+
+	    ipv6 = ipv4 = true if !ipv6 && !ipv4
+	    $ipv4_stack &&= ipv4
+	    $ipv6_stack &&= ipv6
+
 	    true
 	end
 
 	def interact(p, c, tm)
-	    puts "Welcome to Zonecheck #{$zc_version}"
-	    puts 
-	    puts "For more information visit about ZoneCheck visit the"
-	    puts "following URL:"
-	    puts "  http://www.zonecheck.fr/"
-	    puts
+	    puts $mc.get('input_inetd_welcome').gsub('VERSION', ZC_VERSION)
 
 	    print @prompt
 	    $stdout.flush
 	    
-	    
-	    p.transp = "ipv4"
 	    while line = $stdin.gets do
 		line.strip!
 		case line
 	        # Set
+		when /^preset\s+(\w+)$/
+		    case $1
+		    when 'classic'
+			p.verbose = "i,x,d,c"
+		    end
 		when /^set\s+(\w+)\s+(.*)$/
 		    case $1
 		    when 'verbose'	then p.verbose		= $2
@@ -128,9 +202,9 @@ module Input
 			    end
 			rescue ArgumentError
 			end
-		    when 'ns'		then p.domain.ns	= $2
-		    when 'zone','domain' then p.domain.name	= $2
 		    end
+		when /^nslist\s+(.*)/		then p.domain.ns	= $1
+		when /^(?:zone|domain)\s+(.*)/	then p.domain.name	= $1
 		# Unset
 		when /^unset\s+(\w+)$/
 		    case $1
@@ -139,68 +213,13 @@ module Input
 		    end
 		#
 		when /^(?:\?|help)$/
-		    puts <<EOT
-
-Command           Args
-  ?|help           0     This message
-  q|quit|exit      0     Leave ZoneCheck
-  check            0     Launch the zone checking process
-  zone|domain      1     Zone to test
-  nslist           0+    Set the list of the zone nameservers
-  set              1-2   Option to set (see options)
-  unset            1     Option to unset (see options)
-
-Options           Args
-  lang             1     Select another language (en, fr, ...)
-  one              0     Only display the most relevant message
-  quiet            0     Don't display extra titles
-  tagonly          0     Display only tag (suitable for scripting)
-  category         1+    Only perform test for the specified category
-  verbose          1+    Display extra information (see verbose)
-  output           1+    Output (see output)
-  error            1+    Behaviour in case of error (see error)
-  transp           1+    Transport/routing layer (see transp)
-  option           1+    Set extra options (-,-opt,opt,opt=foo)
-
-Arguments for Options
-  verbose:             [intro/testname/explain/details]
-                       [reportok|fatalonly] [testdesc|counter]
-    intro          [i]  Print summary for domain and associated nameservers
-    testname       [n]  Print the test name
-    explain        [x]  Print an explanation for failed tests
-    details        [d]  Print a detailed description of the failure
-    reportok       [o]  Still report passed test
-    fatalonly      [f]  Print fatal errors only
-    testdesc       [t]  Print the test description before running it
-    counter        [c]  Print a test counter
-
-  output:               [byseverity|byhost] [text|html]
-    byseverity    *[bs] Output is sorted/merged by severity
-    byhost         [bh] Output is sorted/merged by host
-    text          *[t]  Output plain text
-    html           [h]  Output HTML
-
-  error:                [allfatal|allwarning|dfltseverity] [stop|nostop]
-    allfatal       [af] All error are considered fatal
-    allwarning     [aw] All error are considered warning
-    dfltseverity  *[ds] Use the severity associated with the test
-    stop          *[s]  Stop on the first fatal error
-    nostop         [ns] Never stop (even on fatal error)
-
-  transp:               [ipv4/ipv6] [udp|tcp|std]
-    ipv4          *[4]  Use IPv4 routing protocol
-    ipv6          *[6]  Use IPv6 routing protocol
-    udp            [u]  Use UDP transport layer
-    tcp            [t]  Use TCP transport layer
-    std           *[s]  Use UDP with fallback to TCP for truncated messages
-
-EOT
+		    puts $mc.get('input_inetd_help')
 		# Leave interaction loop
 		when /^check$/			then return true
 		when /^(?:quit|q|exit)$/	then return false
 
 		else
-		    puts "I didn't understand, try asking for help!"
+		    puts $mc.get('input_inetd_what')
 		end
 
 		print @prompt
@@ -211,7 +230,7 @@ EOT
 	end
 
 	def usage(errcode, io=$stdout)
-	    io.puts $mc.get('input_cgi_usage')
+	    io.puts $mc.get('input_inetd_usage').gsub('PROGNAME', PROGNAME)
 	    exit errcode unless errcode.nil?
 	end
 
