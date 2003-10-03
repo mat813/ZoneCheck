33607f0d17f72c34b7c86b9e0f7938e3656a4d98
Stéphane D'Alu <sdalu>
2003-10-03 13:18:20 +0000

    form can be submitted by pressing enter

--- www/js/formvalidation.js
+++ www/js/formvalidation.js
@@ -35,23 +35,17 @@ function zc_form_setlocale(emptyzone) {
   zc_form_l_emptyzone = emptyzone;
 }
 
-function zc_form_valid(form) {
+function zc_form_validate(form) {
   var zone = form["zone"].value;
   zone = zone.replace(/^\s+/,'').replace(/\s+$/,'');
 
   if (zone.length == 0) {
     alert(zc_form_l_emptyzone);
-    return 0;
+    return false;
   } else {
-    return 1;
+    return true;
   }
 }
-
-function zc_form_check(form) {
-  if (zc_form_valid(form))
-    form.submit();
-  return 0;
-}	  
 
 function zc_form_clear(form) {
   var i;
