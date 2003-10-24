94294d2c5bf795d6960436de85bf19c185f4d3b9
Stéphane D'Alu <sdalu>
2003-10-24 13:55:25 +0000

    New installtion procedure (only need ruby to install)

--- Makefile
+++ Makefile
@@ -1,181 +1,39 @@
 # $Id$
 
+#  
+# CONTACT     : zonecheck@nic.fr 
+# AUTHOR      : Stephane D'Alu <sdalu@nic.fr> 
 # 
-# CONTACT     : zonecheck@nic.fr
-# AUTHOR      : Stephane Bortzmeyer <bortzmeyer@nic.fr>
-#
-# CREATED     : 2002/10/10 16:38:17
-# REVISION    : $Revision$ 
-# DATE        : $Date$
-#
-# CONTRIBUTORS: (see also CREDITS file)
-#   Stephane D'Alu <sdalu@nic.fr>
-#
-#
-# LICENSE     : GPL v2 (or MIT/X11-like after agreement)
-# COPYRIGHT   : AFNIC (c) 2003
-#
-# This file is part of ZoneCheck.
-#
-# ZoneCheck is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# CREATED     : 2003/10/23 21:04:09 
+# REVISION    : $Revision$  
+# DATE        : $Date$ 
 # 
-# ZoneCheck is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with ZoneCheck; if not, write to the Free Software Foundation,
-# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-#
 
-#
-# TODO: don't do install-cgi if INPUT_METHODS doesn't support cgi ??
-#
-
-PREFIX    ?= /usr/local
-RUBY      ?= $(shell which ruby)
-HTML_PATH ?= /zc
-
-LIBEXEC=$(PREFIX)/libexec
-BINDIR=$(PREFIX)/bin
-MANDIR=$(PREFIX)/man
-DOCDIR=$(PREFIX)/share/doc
-ETCDIR=$(PREFIX)/etc
-CGIDIR=$(LIBEXEC)/zc/cgi-bin
-
-INSTALL=install
-#LN=ln -s		# Apache doesn't like symlink for CGI
-LN=ln			# Doesn't cross partition boundary
-#LN=install		# Duplicate file
-CP=cp
-CHMOD=chmod
-FIND=find
-TAR=tar
-
-HTML2TXT=elinks -dump
+RUBY ?=ruby
+ZC_INSTALLER=$(RUBY) ./installer.rb
 
 
-TOPDIR := $(shell if [ "$$PWD" != "" ]; then echo $$PWD; else pwd; fi)
+all: configinfo
 
-ifndef XML_CATALOG_FILES
-XML_CATALOG_FILES=$(TOPDIR)/doc/misc/catalog.xml
-export XML_CATALOG_FILES
-endif
-
-
-all: zc-bin
-
-zc-bin: 
+configinfo: 
 	@echo "Nothing to make, you can install it right now!"
+	@echo " => but ensure that you have the full path for the ruby interpreter!"
 	@echo ""
-	@echo "Default values are:"
-	@echo "  HTML_PATH=$(HTML_PATH)"
-	@echo "  PREFIX=$(PREFIX)"
-	@echo "  RUBY=$(RUBY)"
+	@$(ZC_INSTALLER) configinfo
 	@echo ""
 	@echo "You can change them by using the syntax:"
 	@echo "  $(MAKE) key=value"
 
-zc-cgi:
-	@echo "Not automatized yet"
+install: install-all
 
-
-install: install-common install-cli install-cgi install-doc
-
-
-install-common:
-	@echo "==> Installing core components"
-	$(INSTALL) -d $(LIBEXEC)/zc
-	$(TAR) cf - zc     | (cd $(LIBEXEC)/zc && $(TAR) xvf -)
-	$(RUBY) -p -i \
-		-e "\$$_.gsub!(/^#!.*ruby/, '#!$(RUBY)')" \
-		-e "\$$_.gsub!(/^(ZC_INSTALL_PATH\s*=\s*).*/, '\1\"$(LIBEXEC)/zc\"')" \
-		-e "\$$_.gsub!(/^(ZC_CONFIG_DIR\s*=\s*).*/,   '\1\"$(ETCDIR)/zonecheck\"')" \
-		-e "\$$_.gsub!(/^(ZC_LOCALIZATION_DIR\s*=\s*).*/, '\1\"$(LIBEXEC)/zc/locale\"')" \
-		-e "\$$_.gsub!(/^(ZC_TEST_DIR\s*=\s*).*/,  '\1\"$(LIBEXEC)/zc/test\"')" \
-		-e "\$$_.gsub!(/^(ZC_HTML_PATH\s*=\s*).*/, '\1\"$(HTML_PATH)\"')" \
-		$(LIBEXEC)/zc/zc/zc.rb
-	$(CHMOD) 755 $(LIBEXEC)/zc/zc/zc.rb 
-
-	@echo "==> Installing libraries"
-	$(TAR) cf - lib    | (cd $(LIBEXEC)/zc && $(TAR) xvf -)
-	@echo
-
-	@echo "==> Installing tests"
-	$(TAR) cf - test   | (cd $(LIBEXEC)/zc && $(TAR) xvf -)
-	@echo
-
-	@echo "==> Installing locale"
-	$(TAR) cf - locale | (cd $(LIBEXEC)/zc && $(TAR) xvf -)
-	@echo
-
-	@echo "==> Installing default configuration file"
-	$(INSTALL) -d $(ETCDIR)/zonecheck
-	$(INSTALL) -b -m 0644 etc/zonecheck/zc.conf $(ETCDIR)/zonecheck
-	$(INSTALL) -b -m 0644 etc/zonecheck/fr.rules $(ETCDIR)/zonecheck
-	$(INSTALL) -b -m 0644 etc/zonecheck/de.rules $(ETCDIR)/zonecheck
-	$(INSTALL) -b -m 0644 etc/zonecheck/default.rules $(ETCDIR)/zonecheck
-	$(INSTALL) -b -m 0644 etc/zonecheck/reverse.rules $(ETCDIR)/zonecheck
-	$(INSTALL) -b -m 0644 etc/zonecheck/rootservers $(ETCDIR)/zonecheck
-	@echo "*************************"
-	@echo "** If you already had a zc.conf file it has been renamed"
-	@echo "**   to zc.conf.old"
-	@echo "** Don't forget to edit the zc.conf to reflect your system"
-	@echo "**   configuration"
-	@echo "*************************"
-	@echo
-
-install-cgi:
-	@echo "==> Installing HTML pages"
-	$(TAR) cf - www   | (cd $(LIBEXEC)/zc && $(TAR) xvf -)
-	@echo "==> Patching HTML pages"
-	$(FIND) $(LIBEXEC)/zc/www -name '*.html.*' -exec $(RUBY) -p -i -e "\$$_.gsub!(/HTML_PATH/, '$(HTML_PATH)')" {} \;
-	$(FIND) $(LIBEXEC)/zc/www -name '*.html' -exec $(RUBY) -p -i -e "\$$_.gsub!(/HTML_PATH/, '$(HTML_PATH)')" {} \;
-	@echo
-
-	@echo "==> Installing CGI"
-	$(INSTALL) -d $(CGIDIR)
-	$(LN) -f $(LIBEXEC)/zc/zc/zc.rb $(CGIDIR)/zc.cgi
-	@echo
+install-all:
+	@$(ZC_INSTALLER) all
 
 install-cli:
-	@echo "==> Installing CLI"
-	$(INSTALL) -d $(BINDIR)
-	$(LN) -f $(LIBEXEC)/zc/zc/zc.rb $(BINDIR)/zc
-	$(LN) -f $(LIBEXEC)/zc/zc/zc.rb $(BINDIR)/zonecheck
-	$(INSTALL) -d $(MANDIR)/man1
-	$(INSTALL) -m 0644 man/zonecheck.1 $(MANDIR)/man1/zc.1
-	$(INSTALL) -m 0644 man/zonecheck.1 $(MANDIR)/man1/zonecheck.1
-	@echo
+	@$(ZC_INSTALLER) cli
+
+install-cgi:
+	@$(ZC_INSTALLER) cgi
 
 install-doc:
-	@echo "==> Installing documentation"
-	$(INSTALL) -d $(DOCDIR)/zc
-	$(INSTALL) -m 0644 README TODO INSTALL BUGS $(DOCDIR)/zc
-	@echo
-
-
-
-
-realclean:
-	find . -type f -name '*~' -exec rm {} \;
-	rm -Rf $(TOPDIR)/doc/tmp
-	rm -Rf $(TOPDIR)/doc/html
-
-FAQ: doc/xml/FAQ.xml doc/xml/common/faq.xml doc/misc/html.xsl
-	rm -Rf $(TOPDIR)/doc/tmp/FAQ
-	mkdir -p $(TOPDIR)/doc/tmp/FAQ
-	(cd $(TOPDIR)/doc/tmp/FAQ && xsltproc -nonet $(TOPDIR)/doc/misc/html.xsl $(TOPDIR)/doc/xml/FAQ.xml > FAQ.html)
-	$(HTML2TXT) doc/tmp/FAQ/FAQ.html > FAQ
-
-distrib: FAQ
-
-doc-html: doc/xml/zc.xml doc/xml/common/faq.xml doc/misc/html.xsl
-	rm -Rf $(TOPDIR)/doc/html
-	mkdir -p $(TOPDIR)/doc/html
-	(cd $(TOPDIR)/doc/html && xsltproc -nonet $(TOPDIR)/doc/misc/html.xsl $(TOPDIR)/doc/xml/zc.xml)
-	cp doc/misc/docbook.css doc/html/
+	@$(ZC_INSTALLER) doc
---
+++ installer.rb
@@ -0,0 +1,239 @@
+# $Id$
+
+# 
+# CONTACT     : zonecheck@nic.fr
+# AUTHOR      : Stephane D'Alu <sdalu@nic.fr>
+#
+# CREATED     : 2003/10/23 21:04:09
+# REVISION    : $Revision$ 
+# DATE        : $Date$
+#
+
+
+require 'fileutils'
+include FileUtils
+
+class Installer
+    def initialize
+	interpreter = ENV['_']
+	if RUBY_PLATFORM =~ /mswin32/ 
+	    getcli = Win32API::new('kernel32', "GetCommandLine", [], 'P')
+	    getcli.call() =~ /^\"([^\"]+)\"/
+	    interpreter = $1
+	end
+
+	ARGV.delete_if { |arg|
+	    case arg
+	    when /^-D(\w+)(?:=(.*))?$/ then ENV[$1] = $2   ; true
+	    when /^-U(\w+)$/           then ENV.delete($1) ; true
+	    else false
+	    end
+	}
+
+	ENV['RUBY'      ] ||= interpreter || 'ruby'
+	ENV['PREFIX'    ] ||= '/usr/local'
+	ENV['PROGNAME'  ] ||= 'zonecheck'
+	ENV['HTML_PATH' ] ||= "/#{ENV['PROGNAME']}"
+	ENV['ETCDIST'   ] ||= '-dist'
+	ENV['CHROOT'    ] ||= ''
+
+	ENV['LIBEXEC'   ] ||= "#{ENV['PREFIX']}/libexec"
+	ENV['BINDIR'    ] ||= "#{ENV['PREFIX']}/bin"
+	ENV['MANDIR'    ] ||= "#{ENV['PREFIX']}/man"
+	ENV['DOCDIR'    ] ||= "#{ENV['PREFIX']}/share/doc"
+	ENV['ETCDIR'    ] ||= "#{ENV['PREFIX']}/etc"
+	ENV['CGIDIR'    ] ||= "#{ENV['LIBEXEC']}/#{ENV['PROGNAME']}/cgi-bin"
+
+	@installdir    = "#{ENV['LIBEXEC']}/#{ENV['PROGNAME']}"
+	@confdir       = "#{ENV['ETCDIR']}/#{ENV['PROGNAME']}#{ENV['ETCDIST']}"
+	@zc            = "#{@installdir}/zc/zc.rb"
+
+	@ch_installdir = "#{ENV['CHROOT']}#{@installdir}"
+	@ch_confdir    = "#{ENV['CHROOT']}#{@confdir}"
+	@ch_zc         = "#{ENV['CHROOT']}#{@zc}"
+
+	@verbose       = true
+    end
+
+
+
+
+    def configinfo
+	puts "Default values are:"
+	[ 'RUBY', 'PREFIX', 'PROGNAME', 'HTML_PATH' ]. each { |k|
+	    puts "  #{k}=#{ENV[k]}" }
+    end
+
+
+
+    def inst_doc
+	puts "==> Installing documentation"
+	mkdir_p "#{ENV['CHROOT']}#{ENV['DOCDIR']}/#{ENV['PROGNAME']}",
+	    						:verbose => @verbose
+	install ['README', 'TODO', 'INSTALL', 'BUGS'], 
+	    "#{ENV['CHROOT']}#{ENV['DOCDIR']}/#{ENV['PROGNAME']}",
+	    :mode => 0644,				:verbose => @verbose
+	puts
+    end
+
+
+    def patch_common
+	puts "==> Patching core components"
+	zc_content = File.readlines(@ch_zc)
+	[   [ /^\#!.*ruby/, "#!#{ENV['RUBY']}" ],
+	    [ 'ZC_INSTALL_PATH', "\\1\"#{@installdir}\"" ],
+	    [ 'ZC_CONFIG_DIR', "\\1\"#{ENV['ETCDIR']}/#{ENV['PROGNAME']}\"" ],
+	    [ 'ZC_LOCALIZATION_DIR', "\\1\"#{@installdir}/locale\"" ],
+	    [ 'ZC_TEST_DIR',  "\\1\"#{@installdir}/test\"" ],
+	    [ 'ZC_HTML_PATH', "\\1\"#{ENV['HTML_PATH']}\"" ] ].each { |pattern, value|
+	    zc_content.each { |line|
+		case pattern
+		when Regexp
+		    line.gsub!(pattern, value)
+		when String
+		    line.gsub!(/^(#{pattern}\s*=\s*).*/, value)
+		end
+	    }
+	}
+	File::open(@ch_zc, "w") { |io| io.puts zc_content } 
+	chmod 0755, @ch_zc,				:verbose => @verbose
+	puts
+    end
+
+    def inst_common
+	puts "==> Installing core components"
+	mkdir_p	@ch_installdir,				:verbose => @verbose
+	cp_r	"zc", @ch_installdir,			:verbose => @verbose
+	chmod 0755, @ch_zc,				:verbose => @verbose
+	puts
+
+	puts "==> Installing libraries"
+	cp_r 'lib', @ch_installdir,			:verbose => @verbose
+	puts
+
+	puts "==> Installing tests"
+	cp_r 'test', @ch_installdir,			:verbose => @verbose
+	puts
+
+	puts "==> Installing locale"
+	cp_r 'locale', @ch_installdir,			:verbose => @verbose
+	puts
+
+	puts "==> Installing default configuration file"
+	mkdir_p @ch_confdir,				:verbose => @verbose
+	cp 'etc/zonecheck/zc.conf',      @ch_confdir,	:verbose => @verbose
+	cp 'etc/zonecheck/rootservers',  @ch_confdir,	:verbose => @verbose
+	cp Dir['etc/zonecheck/*.rules'], @ch_confdir,	:verbose => @verbose
+	puts
+    end
+
+
+    def inst_cli
+	puts "==> Installing CLI"
+	mkdir_p "#{ENV['CHROOT']}#{ENV['BINDIR']}",	:verbose => @verbose
+	ln_s @zc, "#{ENV['CHROOT']}#{ENV['BINDIR']}/#{ENV['PROGNAME']}",
+	    :force => true,				:verbose => @verbose
+	mkdir_p "#{ENV['CHROOT']}#{ENV['MANDIR']}/man1",:verbose => @verbose
+	install "man/zonecheck.1",
+	    "#{ENV['CHROOT']}#{ENV['MANDIR']}/man1/#{ENV['PROGNAME']}.1",
+	    :mode => 0644,				:verbose => @verbose
+	puts
+    end
+
+
+    def patch_cgi
+	puts "==> Patching HTML pages"
+	Dir["#{@ch_installdir}/www/*.html.*"].each { |page|
+	    page_content = File.readlines(page)
+	    page_content.each { |line| 
+		line.gsub!(/HTML_PATH/, ENV['HTML_PATH']) }
+	    File::open(page, "w", 0644) { |io| io.puts page_content }
+	}
+	puts
+    end
+
+    def inst_cgi
+	puts "==> Installing HTML pages"
+	cp_r "www", @ch_installdir,			:verbose => @verbose
+	puts
+
+	puts "==> Installing CGI"
+	mkdir_p "#{ENV['CHROOT']}#{ENV['CGIDIR']}",	:verbose => @verbose
+	ln_s @zc, "#{ENV['CHROOT']}#{ENV['CGIDIR']}/zc.cgi",
+	    :force => true,				:verbose => @verbose
+	puts
+    end
+
+
+
+    def info
+	puts "==> Info"
+	unless ENV['ETCDIST'].empty?
+print <<EOT
+- ZoneCheck configuration files have been installed in a distribution
+   specific directory, so that you can merge them with your current
+   configuration.
+  If it is the first time you install ZoneCheck you can simply do:
+   mv #{@confdir} #{ENV['ETCDIR']}/#{ENV['PROGNAME']}
+EOT
+	end
+	puts ""
+        puts "- You are now ready to use ZoneCheck"
+	puts ""
+    end
+
+
+
+    #-- [RULES] -----------------------------------------------------------
+
+    def rule_all
+	inst_common ; patch_common 
+	inst_cli
+	inst_cgi    ; patch_cgi
+	inst_doc
+    end
+    def rule_cli
+	inst_common ; patch_common
+	inst_cli
+    end
+    def rule_cgi
+	inst_common ; patch_common
+	inst_cgi    ; patch_cgi
+    end
+    def rule_doc
+	inst_doc
+    end
+
+    alias rule_configinfo configinfo
+
+end 
+
+
+#----------------------------------------------------------------------
+
+
+#
+# Sanity check
+#
+m = /^(\d+)\.(\d+)\./.match(RUBY_VERSION)
+if (m[1].to_i <= 1) && (m[2].to_i < 8)
+    $stderr.puts "WARNING: ruby version 1.8.0 at least is required"
+    $stderr.puts "WARNING: Hopping that the one defined in RUBY is more recent"
+end
+
+
+inst = Installer::new
+
+
+ARGV.each { |rule|
+    unless inst.respond_to?("rule_#{rule}")
+	puts "ERROR: No rule '#{rule}' available"
+	exit 1
+    end
+}
+
+ARGV.each { |rule|
+    inst.send "rule_#{rule}"
+}
+
+inst.info
