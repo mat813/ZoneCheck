ed54aa8205fc0a9a76ff0d89a751f35fe54eb2e2
Stéphane D'Alu <sdalu>
2004-01-27 11:02:40 +0000

    allow to specify an alternate installation directory for web page (Mohacsi Janos contrib)

--- installer.rb
+++ installer.rb
@@ -55,16 +55,19 @@ class Installer
 	ENV['DOCDIR'    ] ||= "#{ENV['PREFIX']}/share/doc"
 	ENV['ETCDIR'    ] ||= "#{ENV['PREFIX']}/etc"
 	ENV['CGIDIR'    ] ||= "#{ENV['LIBEXEC']}/#{ENV['PROGNAME']}/cgi-bin"
+        ENV['WWWDIR'    ] ||= "#{ENV['PREFIX']}/www"
 
 	ENV['VERSION'	] ||= VERSION
 
 	@installdir    = "#{ENV['LIBEXEC']}/#{ENV['PROGNAME']}"
 	@confdir       = "#{ENV['ETCDIR']}/#{ENV['PROGNAME']}#{ENV['ETCDIST']}"
 	@zc            = "#{@installdir}/zc/zc.rb"
+	@wwwdir        = "#{ENV['WWWDIR']}/#{ENV['PROGNAME']}"
 
 	@ch_installdir = "#{ENV['CHROOT']}#{@installdir}"
 	@ch_confdir    = "#{ENV['CHROOT']}#{@confdir}"
 	@ch_zc         = "#{ENV['CHROOT']}#{@zc}"
+	@ch_wwwdir     = "#{ENV['CHROOT']}#{@wwwdir}"
 
 	@verbose       = true
     end
@@ -174,7 +177,7 @@ class Installer
 
     def patch_cgi
 	puts "==> Patching HTML pages"
-	Dir["#{@ch_installdir}/www/html/*.html.*"].each { |page|
+	Dir["#{@ch_wwwdir}/www/html/*.html.*"].each { |page|
 	    page_content = File.readlines(page)
 	    page_content.each { |line| 
 		line.gsub!(/HTML_PATH/, ENV['HTML_PATH']) }
@@ -185,7 +188,8 @@ class Installer
 
     def inst_cgi
 	puts "==> Installing HTML pages"
-	cp_r "www", @ch_installdir,			:verbose => @verbose
+	mkdir_p @ch_wwwdir,				:verbose => @verbose
+	cp_r "www", @ch_wwwdir,				:verbose => @verbose
 	puts
 
 	puts "==> Installing CGI"
